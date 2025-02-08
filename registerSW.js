f638999be69144e03085fbfde762b5cff28f7fc9
Maulana Kevin Pradana <maulanakevinp@gmail.com>
2025-02-08 19:03:18 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/shopping-list/sw.js', { scope: '/shopping-list/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
